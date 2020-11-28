cbd28a813b082245b35d60b91d265fd3c4065c9e
jonnystv <jonathan@darefoundation.com>
2020-11-28 14:42:40 +0000

    MVP initial song tests passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -9,3 +9,15 @@ class TestSong(unittest.TestCase):
 
     def setUp(self):
         self.song_1 = Song("Pocket Calculator", "Kraftwerk", "electronic")
+
+
+    def test_song_has_title(self):
+        self.assertEqual("Pocket Calculator", self.song_1.title)
+
+
+    def test_song_has_artist(self):
+        self.assertEqual("Kraftwerk", self.song_1.artist)
+
+
+    def test_song_has_genre(self):
+        self.assertEqual("electronic", self.song_1.genre)
